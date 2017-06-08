4149f02f35fedb1335c3129787c2265f9d0a3966
jhunt10 <jxh074@uark.edu>
2017-06-08 11:07:45 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,4 @@
-import java.awt.Dimension;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-
 public class Main{
-	
-	//private JScrollPane scroll;
-	//private JFrame window;
-	
 	public static void main(String[] args) throws Exception {
  		new Controler();
     }
