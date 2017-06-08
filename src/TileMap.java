e03bb714fe8d919322623c2522abdabf344964f2
jhunt10 <jxh074@uark.edu>
2017-06-08 11:08:37 -0500

    Update TileMap.java

--- src/TileMap.java
+++ src/TileMap.java
@@ -1,33 +1,42 @@
-import java.awt.image.BufferedImage;
-
-public class TileMap {
-	private Tile map[][];
+public class TileMap 
+{
+	private byte map[][];
 	
-	public TileMap(int w, int h){
-		map = new Tile[w][h];
-		for(int y=0; y<h; y++){
-			for(int x=0; x<w; x++){
-				map[x][y] = null;
-			}
+	//---------------------------------------
+	//	Basically just a fancy two dim array
+	//---------------------------------------
+	
+	public TileMap(int w, int h)
+	{
+		map = new byte[w][h];
+		for(int y = 0; y < h; y++)
+		for(int x = 0; x < w; x++)
+		{
+			map[x][y] = -1;
 		}
 	}
 	
-	public Tile get(int x, int y){
+	public byte get(int x, int y)
+	{
 		if(x >= 0 && x < map.length && y >= 0 && y < map[0].length)
 			return map[x][y];
 		else
-			return null;
+			return -1;
 	}
 	
-	public void set(int x, int y, Tile t){
-		map[x][y] = t;
+	public void set(int x, int y, byte t)
+	{
+		if(x >= 0 && x < map.length && y >= 0 && y < map[0].length)
+			map[x][y] = t;
 	}
 	
-	public int getWidth(){
+	public int getWidth()
+	{
 		return map.length;
 	}
 	
-	public int getHeight(){
+	public int getHeight()
+	{
 		return map[0].length;
 	}
 }
