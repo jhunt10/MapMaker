ee46f2c2be7331640d163db4bc33cd9286877b8b
jhunt10 <jxh074@uark.edu>
2017-06-08 11:08:22 -0500

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,9 +1,15 @@
 import java.awt.image.BufferedImage;
 
-public class Tile {
+public class Tile 
+{
 	public byte code;
 	public BufferedImage image, scaledImage;
-	public Tile(byte c, BufferedImage i, BufferedImage s){
+	
+	//---------------------------------------
+	//	Simple class to hold tile data together
+	//---------------------------------------
+	public Tile(byte c, BufferedImage i, BufferedImage s)
+	{
 		code = c; image = i; scaledImage = s;
 	}
 }
